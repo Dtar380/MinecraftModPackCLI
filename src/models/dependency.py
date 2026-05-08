f64a55cccc211c0837b70d5750ae5c06709dc648
Dtar380 <dtar380@icloud.com>
2026-05-08 23:27:25 +0200

    Finished models

--- src/models/dependency.py
+++ src/models/dependency.py
@@ -6,4 +6,20 @@ from dataclasses import dataclass
 @dataclass(slots=True)
 class Dependency:
     project_id: str | None
+    version_id: str | None
     dependency_type: str
+
+    @classmethod
+    def from_dict(cls, dependency_data: dict) -> Dependency:
+        return Dependency(
+            project_id=dependency_data["project_id"],
+            version_id=dependency_data["version_id"],
+            dependency_type=dependency_data["dependency_type"],
+        )
+
+    def to_dict(self) -> dict:
+        return {
+            "project_id": self.project_id,
+            "version_id": self.version_id,
+            "dependency_type": self.dependency_type,
+        }
--- src/models/mod.py
+++ src/models/mod.py
@@ -22,3 +22,46 @@ class Mod:
     dependencies: list[Dependency] = field(default_factory=list)
 
     is_library: bool = False
+
+    @classmethod
+    def from_modrinth(
+        cls, project_data: dict, version_data: dict, file_path: Path
+    ) -> Mod:
+        return cls(
+            name=project_data["title"],
+            hash=version_data["files"][0]["hashes"]["sha1"],
+            project_id=project_data["id"],
+            version_id=version_data["id"],
+            client_side=project_data["client_side"],
+            server_side=project_data["server_side"],
+            file_path=file_path,
+            dependencies=[
+                Dependency.from_dict(dep)
+                for dep in version_data.get("dependencies", [])
+            ],
+        )
+
+    @property
+    def client_required(self) -> bool:
+        return self.client_side != "unsupported"
+
+    @property
+    def server_required(self) -> bool:
+        return self.server_side == "required"
+
+    def __str__(self) -> str:
+        return f"{self.name}: version_id={self.version_id}"
+
+    def to_dict(self) -> dict:
+        return {
+            "name": self.name,
+            "hash": self.hash,
+            "project_id": self.project_id,
+            "version_id": self.version_id,
+            "client_side": self.client_side,
+            "sever": self.server_side,
+            "source": "dependency" if self.is_library else "seed",
+            "dependencies": [
+                dep.to_dict() for dep in self.dependencies
+            ]
+        }
