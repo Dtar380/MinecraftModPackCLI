c45dac270e1bba992f8000d570d72454e0ea3239
Dtar380 <dtar380@icloud.com>
2026-05-13 23:16:36 +0200

    Wrote all methods for ConfigManager

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -9,6 +9,7 @@ from __future__ import annotations
 
 # === LOCAL ===
 from .builder import Builder
+from .config import ConfigManager
 
 #  === __all__ definition ===
-__all__ = ["Builder"]
+__all__ = ["Builder", "ConfigManager"]
